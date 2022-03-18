f255869fd0a60d97fd8871803dec07c46282e75b
Sayed Mohammad Ali Mirkazemi <esmirk.137@gmail.com>
2022-03-18 20:06:59 +0330

    config properties

--- src/main/java/ir/blacksparrow/websitebackend/WebsiteBackendApplication.java
+++ src/main/java/ir/blacksparrow/websitebackend/WebsiteBackendApplication.java
@@ -8,6 +8,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static ir.blacksparrow.websitebackend.constant.Constant.EmailSender.*;
+
 @SpringBootApplication
 @EnableSwagger2
 public class WebsiteBackendApplication {
@@ -23,6 +25,9 @@ public class WebsiteBackendApplication {
 
     @Bean
     public JavaMailSender javaMailSender(){
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl javaMailSender= new JavaMailSenderImpl();
+//        javaMailSender.setUsername(SENDER_EMAIL_ADDRESS);
+//        javaMailSender.setPassword(SENDER_EMAIL_PASSWORD);
+        return javaMailSender;
     }
 }
--- src/main/java/ir/blacksparrow/websitebackend/business/sevice/emailSender/IEmailSenderService.java
+++ src/main/java/ir/blacksparrow/websitebackend/business/sevice/emailSender/IEmailSenderService.java
@@ -1,5 +1,7 @@
 package ir.blacksparrow.websitebackend.business.sevice.emailSender;
 
+import ir.blacksparrow.websitebackend.business.dto.UserDto;
+
 public interface IEmailSenderService {
-    void send(String to, String email);
+    void send(UserDto userDto, String token);
 }
